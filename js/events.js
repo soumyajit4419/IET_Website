783d23f34daa53ef897cda4fd04db7f7e08b253e
Tanmay Sinha <alokratn001@gmail.com>
2020-07-17 13:42:57 +0530

    fixed a small bug in eventsjs

--- js/events.js
+++ js/events.js
@@ -20,13 +20,13 @@ $.ajax({
             <div class="modal-dialog modal-dialog-centered" role="">
               <div class="modal-content">
                 <div class="modal-header">
-                  <h5 class="modal-title" id="event6ModalTitle">${res.events[i].eventName}</h5>
+                  <h5 class="modal-title" id="event${i}ModalTitle">${res.events[i].eventName}</h5>
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                   </button>
                 </div>
                 <div class="modal-body  text-center">
-                  <img src="./images/events/${res.events[i].imgUrl}" alt="event6 poster" class="model-img img-fluid">
+                  <img src="./images/events/${res.events[i].imgUrl}" alt="event${i} poster" class="model-img img-fluid">
                 </div>
                 <div class=" modal-footer">
                   <p>${res.events[i].eventBody}</p>
