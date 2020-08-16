027aba02dcbe23484967b2784e64a8379993edd3
soumyajit4419 <ashish454570@gmail.com>
2020-08-16 08:15:12 +0530

    upadted content and links

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# IET_Website
+# IET Website
 Offical Website for IET, Bit Mesra
 
-
-### Contribution Guidelines
-* To prevent merge conflicts. Dont make changes into master. Create a branch, make changes and once changes are done merge them into master.
-* For convience create an issue on the section you are working. So that others dont work on this section.
-* If you are working on Events section create an issue naming `Events` and close the issue once the section is complete.
-* Do add all the info about the using templates for others help.
-
-### Info about templates
-#### Presently used template:-
-* [https://preview.uideck.com/items/plain/](https://preview.uideck.com/items/plain/)
-
-#### Selected Templates for further work:-
-* [https://preview.uideck.com/items/stack/](https://preview.uideck.com/items/stack/)
-* [https://www.opencolony.com/](https://www.opencolony.com/)
-
-#### Repo for finding more templates:-
-* [https://github.com/bradtraversy/design-resources-for-developers](https://github.com/bradtraversy/design-resources-for-developers)
-
-
-### Old website link
-* [https://github.com/aryanAbhishek18/IET-Website](https://github.com/aryanAbhishek18/IET-Website)
--- js/main.js
+++ js/main.js
@@ -1,222 +1,221 @@
 $(function () {
+  "use strict";
 
-    "use strict";
+  //===== Prealoder=====//
 
-    //===== Prealoder=====//
+  $(window).on("load", function (event) {
+    $(".preloader").delay(400).fadeOut(400);
+  });
 
-    $(window).on('load', function (event) {
-        $('.preloader').delay(400).fadeOut(400);
+  //===== Sticky=====//
+
+  $(window).on("scroll", function (event) {
+    var scroll = $(window).scrollTop();
+    if (scroll < 20) {
+      $(".header_navbar").removeClass("sticky");
+    } else {
+      $(".header_navbar").addClass("sticky");
+    }
+  });
+
+  //===== Section Menu Active =====//
+
+  var scrollLink = $(".page-scroll");
+  // Active link switching
+  $(window).scroll(function () {
+    var scrollbarLocation = $(this).scrollTop();
+
+    scrollLink.each(function () {
+      // var sectionOffset = $(this.hash).offset().top - 73;  <- $(this.hash) was causing error.
+      // var sectionOffset = $($(this).attr('href')).offset().top - 73;
+      var sectionOffset = $(this.hash).offset().top - 73;
+
+      if (sectionOffset <= scrollbarLocation) {
+        $(this).parent().addClass("active");
+        $(this).parent().siblings().removeClass("active");
+      }
     });
+  });
 
+  //===== close navbar-collapse when a  clicked=====//
 
-    //===== Sticky=====//
+  $(".navbar-nav a").on("click", function () {
+    $(".navbar-collapse").removeClass("show");
+  });
 
-    $(window).on('scroll', function (event) {
-        var scroll = $(window).scrollTop();
-        if (scroll < 20) {
-            $(".header_navbar").removeClass("sticky");
-        } else {
-            $(".header_navbar").addClass("sticky");
-        }
-    });
+  $(".navbar-toggler").on("click", function () {
+    $(this).toggleClass("active");
+  });
 
+  $(".navbar-nav a").on("click", function () {
+    $(".navbar-toggler").removeClass("active");
+  });
 
-    //===== Section Menu Active =====//
+  //===== slick Events=====//
 
-    var scrollLink = $('.page-scroll');
-    // Active link switching
-    $(window).scroll(function () {
-        var scrollbarLocation = $(this).scrollTop();
+  // $('.work_active').slick({
+  //     dots: true,
+  //     arrows: false,
+  //     infinite: true,
+  //     speed: 800,
+  //     autoplay: true,
+  //     autoplaySpeed: 5000,
+  //     centerMode: true,
+  //     centerPadding: '360px',
+  //     slidesToShow: 2,
+  //     slidesToScroll: 1,
+  //     responsive: [
+  //         {
+  //             breakpoint: 1600,
+  //             settings: {
+  //                 slidesToShow: 2,
+  //                 centerPadding: '400px',
+  //             }
+  //         },
+  //         {
+  //             breakpoint: 1400,
+  //             settings: {
+  //                 slidesToShow: 2,
+  //                 centerPadding: '160px',
+  //             }
+  //         },
+  //         {
+  //             breakpoint: 1200,
+  //             settings: {
+  //                 slidesToShow: 2,
+  //                 centerPadding: '80px',
+  //             }
+  //         },
+  //         {
+  //             breakpoint: 992,
+  //             settings: {
+  //                 slidesToShow: 2,
+  //                 centerPadding: '0',
+  //                 centerMode: false,
+  //             }
+  //         },
+  //         {
+  //             breakpoint: 768,
+  //             settings: {
+  //                 slidesToShow: 1,
+  //                 centerPadding: '0',
+  //                 centerMode: false,
+  //             }
+  //         },
+  //         {
+  //             breakpoint: 576,
+  //             settings: {
+  //                 slidesToShow: 1,
+  //                 centerPadding: '0',
+  //                 centerMode: false,
+  //             }
+  //         }
+  //     ]
+  // });
 
-        scrollLink.each(function () {
+  // "event4": {},
+  // "event5": {},
+  // "event6": {},
+  // "event7": {},
+  // "event8": {},
+  // "event9": {},
+  // "event10": {},
+  // "event11": {}
 
-            // var sectionOffset = $(this.hash).offset().top - 73;  <- $(this.hash) was causing error.
-            // var sectionOffset = $($(this).attr('href')).offset().top - 73;
-            var sectionOffset = $(this.hash).offset().top - 73;
+  //====== slick Team =====//
 
-            if (sectionOffset <= scrollbarLocation) {
-                $(this).parent().addClass('active');
-                $(this).parent().siblings().removeClass('active');
-            }
-        });
-    });
+  $(".team_active").slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    centerMode: true,
+    centerPadding: "0",
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 
+  //===== Back to top =====//
 
-    //===== close navbar-collapse when a  clicked=====//
+  // Show or hide the sticky footer button
+  $(window).on("scroll", function (event) {
+    if ($(this).scrollTop() > 600) {
+      $(".back-to-top").fadeIn(200);
+    } else {
+      $(".back-to-top").fadeOut(200);
+    }
+  });
 
-    $(".navbar-nav a").on('click', function () {
-        $(".navbar-collapse").removeClass("show");
-    });
+  //Animate the scroll to yop =====//
+  $(".back-to-top").on("click", function (event) {
+    event.preventDefault();
 
-    $(".navbar-toggler").on('click', function () {
-        $(this).toggleClass("active");
-    });
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500
+    );
+  });
 
-    $(".navbar-nav a").on('click', function () {
-        $(".navbar-toggler").removeClass('active');
-    });
+  //=====  WOW active=====//
 
-
-    //===== slick Events=====//
-
-    // $('.work_active').slick({
-    //     dots: true,
-    //     arrows: false,
-    //     infinite: true,
-    //     speed: 800,
-    //     autoplay: true,
-    //     autoplaySpeed: 5000,
-    //     centerMode: true,
-    //     centerPadding: '360px',
-    //     slidesToShow: 2,
-    //     slidesToScroll: 1,
-    //     responsive: [
-    //         {
-    //             breakpoint: 1600,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //                 centerPadding: '400px',
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 1400,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //                 centerPadding: '160px',
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 1200,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //                 centerPadding: '80px',
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 992,
-    //             settings: {
-    //                 slidesToShow: 2,
-    //                 centerPadding: '0',
-    //                 centerMode: false,
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 768,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 centerPadding: '0',
-    //                 centerMode: false,
-    //             }
-    //         },
-    //         {
-    //             breakpoint: 576,
-    //             settings: {
-    //                 slidesToShow: 1,
-    //                 centerPadding: '0',
-    //                 centerMode: false,
-    //             }
-    //         }
-    //     ]
-    // });
-
-    // "event4": {},
-    // "event5": {},
-    // "event6": {},
-    // "event7": {},
-    // "event8": {},
-    // "event9": {},
-    // "event10": {},
-    // "event11": {}
-
-    //====== slick Team =====//
-
-    $('.team_active').slick({
-        dots: true,
-        arrows: false,
-        infinite: true,
-        speed: 800,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        centerMode: true,
-        centerPadding: '0',
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 3,
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                }
-            }
-        ]
-    });
-
-    //===== Back to top =====//
-
-    // Show or hide the sticky footer button
-    $(window).on('scroll', function (event) {
-        if ($(this).scrollTop() > 600) {
-            $('.back-to-top').fadeIn(200)
-        } else {
-            $('.back-to-top').fadeOut(200)
-        }
-    });
-
-
-    //Animate the scroll to yop =====//
-    $('.back-to-top').on('click', function (event) {
-        event.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: 0,
-        }, 1500);
-    });
-
-    //=====  WOW active=====//
-
-    var wow = new WOW({
-        boxClass: 'wow', //
-        mobile: false, // 
-    })
-    wow.init();
+  var wow = new WOW({
+    boxClass: "wow", //
+    mobile: false, //
+  });
+  wow.init();
 });
 
 //=====  Slick Gallery=====//
-$('.testimonial-active').slick({
-    dots: false, arrows: true, prevArrow: '<span class="prev"><i class="fas fa-arrow-left"></i></span>',
-    nextArrow: '<span class="next"><i class="fas fa-arrow-right"></i></span>',
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    speed: 800,
-    slidesToShow: 1,
+$(".testimonial-active").slick({
+  dots: false,
+  arrows: true,
+  prevArrow: '<span class="prev"><i class=""></i></span>',
+  nextArrow: '<span class="next"><i class=""></i></span>',
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  speed: 800,
+  slidesToShow: 1,
 });
 
-// $('.testimonial-active').slick({
-//     dots: false, arrows: true, 
-//     prevArrow: '<span class="prev"><i class="fas fa-arrow-left"></i></span>',
-//     nextArrow: $(this).find(".next"),
-//     infinite: true,
-//     autoplay: true,
-//     autoplaySpeed: 5000,
-//     speed: 800,
-//     slidesToShow: 1,
+// $(".testimonial-active").slick({
+//   dots: false,
+//   arrows: true,
+//   prevArrow: '<span class="prev"><i class="fas fa-arrow-left"></i></span>',
+//   nextArrow: $(this).find(".next"),
+//   infinite: true,
+//   autoplay: true,
+//   autoplaySpeed: 5000,
+//   speed: 800,
+//   slidesToShow: 1,
 // });
