4885f7428ce118821a2ff66f11fe54256270be47
goshrow <abhroindia@gmail.com>
2020-12-07 11:13:01 +0530

    AJAX works properly

--- js/gallery.js
+++ js/gallery.js
@@ -4,8 +4,8 @@ while(j<=20){
     $("#events-gallery").append(`
     <div class="single-testimonial">
     <img src="./images/gallery/events/event${j}.jpg" class="img-fluid"
-        style="border-radius: 10px;" />
-     </div>
+        style="border-radius: 10px;" alt="event" />
+    </div>
     `)
     j++;
 }
@@ -17,7 +17,7 @@ while(i<=19)
     $("#workshop-gallery").append(`
     <div class="single-testimonial">
     <img src="./images/gallery/workshops/workshop${i}.jpeg" class="img-fluid"
-        style="border-radius: 10px;" />
+        style="border-radius: 10px;" alt="workshop" />
      </div>
     `)
     i++;
